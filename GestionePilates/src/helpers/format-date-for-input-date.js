f4c6992b2e09c1942645a400b5c29e8fdb135bf9
Ezequiel <up872640@myport.ac.uk>
2020-03-08 22:10:03 +0000

    Fixed date bug

--- GestionePilates/src/helpers/format-date-for-input-date.js
+++ GestionePilates/src/helpers/format-date-for-input-date.js
@@ -1,5 +1,5 @@
 const formatDate = date => {
-  const day = ('0' + (date.getDay() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
   const month = ('0' + (date.getMonth() + 1)).slice(-2);
   const year = date.getFullYear();
 
