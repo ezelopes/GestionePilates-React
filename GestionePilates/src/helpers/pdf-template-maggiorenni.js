ddc970c5b0139ebae8bd47a0c76ea7e54528c474
Ezequiel <up872640@myport.ac.uk>
2020-03-09 23:35:06 +0000

    Finished receipts templates

--- GestionePilates/src/helpers/pdf-template-maggiorenni.js
+++ GestionePilates/src/helpers/pdf-template-maggiorenni.js
@@ -12,7 +12,12 @@ const pdfTemplateMaggiorenni = async (allievaInfo, ricevutaInfo) => {
   const today = formatDate(new Date(), false);
 
   const docDefinition = {
-    pageMargins: [40, 10, 40, 0],
+    info: {
+      title: `${ricevutaInfo.NumeroRicevuta}_${allievaInfo.Nome}_${allievaInfo.Cognome}_Ricevuta`,
+      author: 'Roxana Carro',
+      subject: `Ricevuta ${ricevutaInfo.NumeroRicevuta} di ${allievaInfo.Nome} ${allievaInfo.Cognome}`
+    },
+    pageMargins: [40, 40, 40, 0],
     content: [
       {
         image: label_logo,
@@ -39,10 +44,7 @@ const pdfTemplateMaggiorenni = async (allievaInfo, ricevutaInfo) => {
       },
       {
         // ${sommaEuroInLettere.ToUpper()}
-        text: `di aver ricevuto dal/dalla Sig./Sig.Ra ${allievaInfo.Nome} ${allievaInfo.Cognome} , C.F. ${allievaInfo.CodiceFiscale},
-          nato/a a ${allievaInfo.LuogoNascita}, il ${allievaInfo.DataNascita}
-          residente in ${allievaInfo.Indirizzo}, ${allievaInfo.Citta}, la somma di ${ricevutaInfo.SommaEuro}€ (${SommaEuroInLettere} Euro) per l'iscrizione al corso di ${allievaInfo.Disciplina} dal ${ricevutaInfo.DataInizio}
-          al ${ricevutaInfo.DataScadenza}`,
+        text: `di aver ricevuto dal/dalla Sig./Sig.Ra ${allievaInfo.Nome} ${allievaInfo.Cognome} , C.F. ${allievaInfo.CodiceFiscale}, nato/a a ${allievaInfo.LuogoNascita}, il ${allievaInfo.DataNascita} residente in ${allievaInfo.Indirizzo}, ${allievaInfo.Citta}, la somma di ${ricevutaInfo.SommaEuro}€ (${SommaEuroInLettere} Euro) per l'iscrizione al corso di ${allievaInfo.Disciplina} dal ${ricevutaInfo.DataInizio} al ${ricevutaInfo.DataScadenza}`,
         alignment: 'center',
         fontSize: 10,
         margin: [0, 0, 0, 15]
@@ -72,9 +74,60 @@ const pdfTemplateMaggiorenni = async (allievaInfo, ricevutaInfo) => {
         margin: [0, 0, 0, 15]
       },
       {
+        canvas: [{ type: 'line', x1: 0, y1: 5, x2: 595 - 2 * 40, y2: 5, lineWidth: 1 }],
+        margin: [0, 0, 0, 15]
+      },
+      {
+        image: label_logo,
+        fit: [100, 100]
+      },
+      {
+        text: `Ricevuta n° ${ricevutaInfo.NumeroRicevuta}`,
+        alignment: 'right',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
         text:
-          '----------------------------------------------------------------------------------------------------------------------',
+          'L’associazione sportiva dilettantistica PIL-ART con sede legale a Stezzano in Via C. Battisti 9°, C.F. 95229530167',
         alignment: 'center',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: 'DICHIARA',
+        alignment: 'center',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        // ${sommaEuroInLettere.ToUpper()}
+        text: `di aver ricevuto dal/dalla Sig./Sig.Ra ${allievaInfo.Nome} ${allievaInfo.Cognome} , C.F. ${allievaInfo.CodiceFiscale}, nato/a a ${allievaInfo.LuogoNascita}, il ${allievaInfo.DataNascita} residente in ${allievaInfo.Indirizzo}, ${allievaInfo.Citta}, la somma di ${ricevutaInfo.SommaEuro}€ (${SommaEuroInLettere} Euro) per l'iscrizione al corso di ${allievaInfo.Disciplina} dal ${ricevutaInfo.DataInizio} al ${ricevutaInfo.DataScadenza}`,
+        alignment: 'center',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: `Stezzano, ${today}`,
+        alignment: 'left',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: 'Il Presidente',
+        alignment: 'right',
+        fontSize: 10,
+        margin: [0, 0, 0, 10]
+      },
+      {
+        text: 'Roxana Carro',
+        alignment: 'right',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: 'Pil Art è affiliata all’ACSI e regolarmente iscritta sul registro del CONI',
+        alignment: 'left',
         fontSize: 8,
         margin: [0, 0, 0, 15]
       }
--- GestionePilates/src/helpers/pdf-template-minorenni.js
+++ GestionePilates/src/helpers/pdf-template-minorenni.js
@@ -12,7 +12,12 @@ const pdfTemplateMinorenni = async (allievaInfo, ricevutaInfo) => {
   const today = formatDate(new Date(), false);
 
   const docDefinition = {
-    pageMargins: [40, 10, 40, 0],
+    info: {
+      title: `${ricevutaInfo.NumeroRicevuta}_${allievaInfo.Nome}_${allievaInfo.Cognome}_Ricevuta`,
+      author: 'Roxana Carro',
+      subject: `Ricevuta ${ricevutaInfo.NumeroRicevuta} di ${allievaInfo.Nome} ${allievaInfo.Cognome}`
+    },
+    pageMargins: [40, 40, 40, 0],
     content: [
       {
         image: label_logo,
@@ -41,17 +46,17 @@ const pdfTemplateMinorenni = async (allievaInfo, ricevutaInfo) => {
         // ${sommaEuroInLettere.ToUpper()}
         text: `di aver ricevuto dal/dalla Sig./Sig.Ra ${allievaInfo.NomeGenitore} ${
           allievaInfo.CognomeGenitore
-        },
-        C.F. ${
+        }, C.F. ${
           allievaInfo.CodiceFiscaleGenitore
         }, il pagamento effetuato tramite ${ricevutaInfo.TipoPagamento.toUpperCase()} equilavente alla somma di ${
           ricevutaInfo.SommaEuro
-        }€ (${SommaEuroInLettere} Euro),
-        per l'iscrizione di ${allievaInfo.Nome} ${allievaInfo.Cognome}, C.F. ${
-          allievaInfo.CodiceFiscale
-        } nato/a a ${allievaInfo.LuogoNascita},
-        il ${allievaInfo.DataNascita} residente in ${allievaInfo.Indirizzo}, ${allievaInfo.Citta}
-        al corso di ${allievaInfo.Disciplina} dal ${ricevutaInfo.DataInizio} al ${ricevutaInfo.DataScadenza}`,
+        }€ (${SommaEuroInLettere} Euro), per l'iscrizione di ${allievaInfo.Nome} ${
+          allievaInfo.Cognome
+        }, C.F. ${allievaInfo.CodiceFiscale} nato/a a ${allievaInfo.LuogoNascita}, il ${
+          allievaInfo.DataNascita
+        } residente in ${allievaInfo.Indirizzo}, ${allievaInfo.Citta} al corso di ${
+          allievaInfo.Disciplina
+        } dal ${ricevutaInfo.DataInizio} al ${ricevutaInfo.DataScadenza}`,
         alignment: 'center',
         fontSize: 10,
         margin: [0, 0, 0, 15]
@@ -88,9 +93,79 @@ const pdfTemplateMinorenni = async (allievaInfo, ricevutaInfo) => {
         margin: [0, 0, 0, 15]
       },
       {
+        canvas: [{ type: 'line', x1: 0, y1: 5, x2: 595 - 2 * 40, y2: 5, lineWidth: 1 }],
+        margin: [0, 0, 0, 15]
+      },
+      {
+        image: label_logo,
+        fit: [100, 100]
+      },
+      {
+        text: `Ricevuta n° ${ricevutaInfo.NumeroRicevuta}`,
+        alignment: 'right',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
         text:
-          '----------------------------------------------------------------------------------------------------------------------',
+          'L’associazione sportiva dilettantistica PIL-ART con sede legale a Stezzano in Via C. Battisti 9°, C.F. 95229530167',
         alignment: 'center',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: 'DICHIARA',
+        alignment: 'center',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        // ${sommaEuroInLettere.ToUpper()}
+        text: `di aver ricevuto dal/dalla Sig./Sig.Ra ${allievaInfo.NomeGenitore} ${
+          allievaInfo.CognomeGenitore
+        }, C.F. ${
+          allievaInfo.CodiceFiscaleGenitore
+        }, il pagamento effetuato tramite ${ricevutaInfo.TipoPagamento.toUpperCase()} equilavente alla somma di ${
+          ricevutaInfo.SommaEuro
+        }€ (${SommaEuroInLettere} Euro), per l'iscrizione di ${allievaInfo.Nome} ${
+          allievaInfo.Cognome
+        }, C.F. ${allievaInfo.CodiceFiscale} nato/a a ${allievaInfo.LuogoNascita}, il ${
+          allievaInfo.DataNascita
+        } residente in ${allievaInfo.Indirizzo}, ${allievaInfo.Citta} al corso di ${
+          allievaInfo.Disciplina
+        } dal ${ricevutaInfo.DataInizio} al ${ricevutaInfo.DataScadenza}`,
+        alignment: 'center',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text:
+          'Si comunica che ai sensi dell-art. 15, comma 1°, lett. I-quinquies del TUIR, le spese, per un importo non superiore a 210 euro all’anno, sostenute per l’iscrizione annuale e l’abbonamento, per i ragazzi di età compresa tra 5 e 18 anni, ad associazioni sportive dilettantistiche sono detraibili nella misura del 19% e che l’associazione risulta in possesso dei requisiti a tal fine richiesti',
+        alignment: 'center',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: `Stezzano, ${today}`,
+        alignment: 'left',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: 'Il Presidente',
+        alignment: 'right',
+        fontSize: 10,
+        margin: [0, 0, 0, 10]
+      },
+      {
+        text: 'Roxana Carro',
+        alignment: 'right',
+        fontSize: 10,
+        margin: [0, 0, 0, 15]
+      },
+      {
+        text: 'Pil Art è affiliata all’ACSI e regolarmente iscritta sul registro del CONI',
+        alignment: 'left',
         fontSize: 8,
         margin: [0, 0, 0, 15]
       }
