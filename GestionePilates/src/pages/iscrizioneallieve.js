3148b18f3dab0cef0f8395e840e9f13f54891f97
ezelopes <up872640@myport.ac.uk>
2020-04-05 14:59:41 +0100

    Removed comments

--- GestionePilates/src/pages/iscrizioneallieve.js
+++ GestionePilates/src/pages/iscrizioneallieve.js
@@ -1,18 +1,12 @@
-import React from 'react'; // useEffect, useState
+import React from 'react';
 import FormCreaAllieva from '../components/form_crea_allieva';
 
 function IscrizioneAllieve() {
   return (
     <>
       <div className="page-body">
-        <h3>ISCRIZIONE</h3>
         <FormCreaAllieva />
       </div>
-      {/* <Link to="/">
-        <Button raised ripple style={{ marginLeft: '5em', marginTop: '2em' }}>
-          Home
-        </Button>
-      </Link> */}
     </>
   );
 }
--- GestionePilates/src/pages/paginaallieve.js
+++ GestionePilates/src/pages/paginaallieve.js
@@ -1,18 +1,10 @@
 import React from 'react';
-// import { Button } from 'react-mdl';
 import ListaAllieve from '../components/lista_allieve';
-// import ListaRicevute from '../components/lista_ricevute';
 
 function PaginaAllieve(/*props*/) {
   return (
-    <div className="paginaallieve-body">
+    <div className="page-body">
       <ListaAllieve />
-      {/* <div style={{ display: 'flex', flexDirection: 'row', marginTop: '30px' }}>
-        <ListaRicevute />
-        <Button raised ripple style={{ marginLeft: '2em', marginTop: '2em' }}>
-          Button
-        </Button>
-      </div> */}
     </div>
   );
 }
