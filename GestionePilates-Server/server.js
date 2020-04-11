7a3c131e8940578956fdf25ee41d64868e9b076d
ezelopes <up872640@myport.ac.uk>
2020-04-11 15:12:44 +0100

    Minor Changes

--- GestionePilates-Server/server.js
+++ GestionePilates-Server/server.js
@@ -7,17 +7,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.json({ extended: false })); // adds `.body` in the request
+// adds `.body` in the request
+app.use(bodyParser.json({ extended: false }));
 
 app.use('/api', indexControllers);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/client')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  })
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'))
+//   })
+// }
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
