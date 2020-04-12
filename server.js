440736eccc5d2f0f45c49d654822c3488a865a2d
ezelopes <up872640@myport.ac.uk>
2020-04-12 16:04:34 +0100

    Added npm run build functionality, with Webpack, extra dependencies and babel loaders.

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const indexControllers = require('./controllers/index');
 const path = require('path');
+const compression = require('compression');
+const indexControllers = require('./controllers/index');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // adds `.body` in the request
+app.use(compression());
 app.use(bodyParser.json({ extended: false }));
 
 app.use('/api', indexControllers);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/client')));
+if (process.env.NODE_ENV === 'production') {
+  const DIST_DIR = path.join(__dirname, 'dist');
+  const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'))
-//   })
-// }
+  app.use(express.static(DIST_DIR));
+
+  app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE)
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
