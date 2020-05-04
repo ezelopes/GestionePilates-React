4721a2115aad358eb79ff5f0613d7cdb498347b0
ezelopes <up872640@myport.ac.uk>
2020-05-05 00:00:35 +0100

    Replacing webpack production with Build React-Scripts

--- server.js
+++ server.js
@@ -15,13 +15,10 @@ app.use(bodyParser.json({ extended: false }));
 app.use('/api', indexControllers);
 
 if (process.env.NODE_ENV === 'production') {
-  const DIST_DIR = path.join(__dirname, 'dist');
-  const HTML_FILE = path.join(DIST_DIR, 'index.html');
-
-  app.use(express.static(DIST_DIR));
-
-  app.get('*', (req, res) => {
-    res.sendFile(HTML_FILE)
+  app.use(express.static(path.join(__dirname, "./build")));
+  
+  app.get('*', function response (req, res) {
+    res.sendFile(path.join(__dirname, "./build", "index.html"));
   })
 }
 
