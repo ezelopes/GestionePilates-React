862503b898069bfff7fa38befd1fa1747be03008
ezelopes <up872640@myport.ac.uk>
2020-07-19 23:16:50 +0100

    Update class to className in navbar component

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,29 +1,29 @@
 import React from 'react';
 
 const NavBar = () => (
-  <nav class="navbar navbar-expand-md navbar-light bg-light">
-    <a href="/" class="navbar-brand"> PilArt </a>
+  <nav className="navbar navbar-expand-md navbar-light bg-light">
+    <a href="/" className="navbar-brand"> PilArt </a>
 
-    <div id="navbarCollapse" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"> Allieva </a>
-            <div class="dropdown-menu">
-                <a href="/paginaallieve" class="dropdown-item"> Lista Allieve </a>
-                <a href="/iscrizioneallieve" class="dropdown-item"> Iscrizione Allieva </a>
-                <a href="/modificaeliminaallieva" class="dropdown-item"> Modifica/Elimina Allieva </a>
+    <div id="navbarCollapse" className="collapse navbar-collapse">
+        <ul className="nav navbar-nav">
+          <li className="nav-item dropdown">
+            <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown"> Allieva </a>
+            <div className="dropdown-menu">
+                <a href="/paginaallieve" className="dropdown-item"> Lista Allieve </a>
+                <a href="/iscrizioneallieve" className="dropdown-item"> Iscrizione Allieva </a>
+                <a href="/modificaeliminaallieva" className="dropdown-item"> Modifica/Elimina Allieva </a>
             </div>
           </li>
-          <li class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"> Insegnanti </a>
-            <div class="dropdown-menu">
-                <a href="/paginainsegnanti" class="dropdown-item"> Lista Insegnanti </a>
-                <a href="/iscrizioneinsegnanti" class="dropdown-item"> Iscrizione Insegnanti </a>
-                <a href="/modificaeliminainsegnante" class="dropdown-item"> Modifica/Elimina Insegnanti </a>
+          <li className="nav-item dropdown">
+            <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown"> Insegnanti </a>
+            <div className="dropdown-menu">
+                <a href="/paginainsegnanti" className="dropdown-item"> Lista Insegnanti </a>
+                <a href="/iscrizioneinsegnanti" className="dropdown-item"> Iscrizione Insegnanti </a>
+                <a href="/modificaeliminainsegnante" className="dropdown-item"> Modifica/Elimina Insegnanti </a>
             </div>
           </li>
-          <li class="nav-item">
-              <a href="/paginaricevute" class="nav-link"> Ricevute </a>
+          <li className="nav-item">
+              <a href="/paginaricevute" className="nav-link"> Ricevute </a>
           </li>
         </ul>
     </div>
