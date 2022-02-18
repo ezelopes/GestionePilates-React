a24beb4415f2a752ca97f2d1648661c5a31fa65b
ezelopes <up872640@myport.ac.uk>
2022-02-18 22:03:58 +0000

    eslint: fix linter

--- database/helpers/index.js
+++ database/helpers/index.js
@@ -1,16 +1,18 @@
 const moment = require('moment');
 
-const isValidDate = (d) => {
-  return d instanceof Date;
-};
+const isValidDate = (d) => d instanceof Date;
 
 const getFormattedDate = (date) => {
-  if (!date) return null;
+  if (!date) {
+    return null;
+  }
 
   const convertedDate = moment(date, moment.ISO_8601).format('YYYY-MM-DD');
   const timestamp = moment(convertedDate, moment.ISO_8601).unix();
 
-  if (!isNaN(timestamp)) return convertedDate;
+  if (!Number.isNaN(timestamp)) {
+    return convertedDate;
+  }
 
   return null;
 };
