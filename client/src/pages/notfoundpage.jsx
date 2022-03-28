105396c63589d7afb5c8bd4a4e8b07801c500904
ezelopes <up872640@myport.ac.uk>
2022-03-28 22:31:15 +0100

    feat: styled not found page

--- client/src/pages/notfoundpage.jsx
+++ client/src/pages/notfoundpage.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
-// TODO: Make a nicer 404 page (maybe ballet dance or something)
-const NotFoundPage = () => <h1> 404: Page Not Found </h1>;
+const NotFoundPage = () => (
+  <div className="not-found-page">
+    <h1> Oops! Ti sei persa? </h1>
+    <div>
+      <Button href="/paginaallieve" size="lg">
+        Torna alla Lista delle Allieve
+      </Button>
+    </div>
+  </div>
+);
 
 export default NotFoundPage;
