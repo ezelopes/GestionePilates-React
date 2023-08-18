13585d198df175fb84113fe8d2ce594f53b67c78
ezelopes <up872640@myport.ac.uk>
2023-08-18 23:50:53 +0100

    feat: controlled fields for student creation

--- client/src/pages/StudentSubscriptionPage.jsx
+++ client/src/pages/StudentSubscriptionPage.jsx
@@ -1,12 +1,79 @@
 import React from 'react';
-import { createStudent } from '../helpers/apiCalls';
+import axios from 'axios';
+import { useMutation } from 'react-query';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Button, Spinner } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 
-import UpsertUserForm from '../components/user/UpsertUserForm';
+import { withReactQuery } from '../components/common/withReactQuery';
+import UserFormFields from '../components/user/UserFormFields';
+import Translation from '../components/common/Translation';
 
-const StudentSubscriptionPage = () => (
-  <div className="form-wrapper subscription-form">
-    <UpsertUserForm isStudent callback={createStudent} isForCreating />
-  </div>
-);
+import { addStudentToStorage } from '../helpers/sessionStorage';
 
-export default StudentSubscriptionPage;
+import { ages, disciplines, schools } from '../commondata';
+import toastConfig from '../commondata/toast.config';
+
+const StudentSubscriptionPage = () => {
+  const defaultValues = {
+    IsAdult: ages[0].value,
+    TaxCode: '',
+    Name: '',
+    Surname: '',
+    City: '',
+    Address: '',
+    MobilePhone: '',
+    Email: '',
+    BirthPlace: '',
+    DOB: undefined,
+    Discipline: disciplines[0].value,
+    Course: undefined,
+    School: schools[0].value,
+    RegistrationDate: undefined,
+    CertificateExpirationDate: undefined,
+    ParentTaxCode: '',
+    ParentName: '',
+    ParentSurname: '',
+  };
+
+  const form = useForm({ defaultValues });
+
+  const { handleSubmit, reset } = form;
+
+  const { mutateAsync, isLoading } = useMutation(async (data) => axios.put('/api/student/createStudent', data), {
+    onSuccess: (response, variables) => {
+      const newStudent = { ...variables, StudentID: response.data.StudentID };
+
+      addStudentToStorage(newStudent);
+
+      reset();
+
+      toast.success(response.data.message, toastConfig);
+    },
+    onError: (err) => toast.error(err.message, toastConfig),
+  });
+
+  return (
+    <div className="form-wrapper subscription-form">
+      <FormProvider {...form}>
+        <form className="create-student-form" id="create-student-form" onSubmit={handleSubmit(mutateAsync)}>
+          <UserFormFields idPrefix="create-student-form" defaultValues={defaultValues} isStudent />
+        </form>
+      </FormProvider>
+      <div className="buttons-container justify-content-center">
+        <Button form="create-student-form" type="submit" variant="success" disabled={isLoading}>
+          {isLoading ? (
+            <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+          ) : (
+            <Translation value="buttons.student.createStudent" />
+          )}
+        </Button>
+        <Button variant="secondary" onClick={() => reset()}>
+          <Translation value="buttons.resetForm" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default withReactQuery(StudentSubscriptionPage);
